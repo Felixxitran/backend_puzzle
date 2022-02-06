aa7fea4335f2989ae110a6b118c1ecfaeefdfa04
Le Quan <minhquanle_2002@yahoo.com>
2022-02-05 18:34:40 -0800

    added randomize functions

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const io = require('socket.io')(3001, {
 io.on('connection', socket => {
     console.log(socket.id);
     socket.on("sendData", data => {
+        checkCorrectPattern(data);
         socket.broadcast.emit("sendData", data);
     })
-})
+});
+
+const checkCorrectPattern = (data) => {
+    //if true do randomizePattern function and send new data
+    //if false just send data to clients
+}
+
+const randomizePattern = (data) => {
+    //return new data
+}
